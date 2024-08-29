6017230f9a60307aa7ee1a30aba1a30619275184
Adriana del Pilar Apaza Tintaya <adrianadlpilar@gmail.com>
2024-08-29 16:12:53 -0500

    rails watch list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,27 @@
 class ListsController < ApplicationController
 
-  def show
-    @bookmark = Bookmark.find(params[:id])
+  def index
+    @lists = List.all
   end
-  
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
